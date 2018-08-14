1254a6158e141aedd2a13f2baec3b8df3fa0d133
Brandon Green <brandon@brandonleeproductions.com>
2018-08-14 23:11:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,14 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
+def initialize
+  @name = name
+  @grade = grade
+  @id = some_id
+end
+
+
+def self.create_table
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
