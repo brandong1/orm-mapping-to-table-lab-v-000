3451da368f08b6705bb6becd8366d8ba9e6d7b25
Brandon Green <brandon@brandonleeproductions.com>
2018-08-14 23:31:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,4 +39,8 @@ attr_reader :id
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+  end
+
 end
