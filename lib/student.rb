491f3abf880642d97cfc5aca1f0647305e663940
Brandon Green <brandon@brandonleeproductions.com>
2018-08-14 23:14:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,9 +18,6 @@ attr_reader :id
         )
         SQL
     DB[:conn].execute(sql)
-  end
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
 
   end
 end
